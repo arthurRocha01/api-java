07d12c71f848082c6d78fefe0028e6e08674008d
Arthur Rocha <arthurrocha007.net@gmail.com>
2026-01-12 17:32:41 -0300

    feat: adiciona repository

--- src/main/java/com/arthurrocha01/api_java/repository/ItemRepository.java
+++ src/main/java/com/arthurrocha01/api_java/repository/ItemRepository.java
@@ -1,7 +1,7 @@
 package com.arthurrocha01.api_java.repository;
 import java.util.List;
 
-import com.arthurrocha01.api_java.domain.item.Item;
+import com.arthurrocha01.api_java.model.item.Item;
 
 public interface ItemRepository {
 	List<Item> findAll();
