410e0417fbcde05c32a7486c8bd84d9201e50817
Arthur Rocha <arthurrocha007.net@gmail.com>
2026-01-12 17:32:04 -0300

    fix: corrige semantica

---
+++ src/main/java/com/arthurrocha01/api_java/model/item/Item.java
@@ -0,0 +1,21 @@
+package com.arthurrocha01.api_java.model.item;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public class Item {
+	private final UUID id;
+	private final String description;
+	private final BigDecimal price;
+	
+	public Item(UUID id, String description, BigDecimal price) {
+		this.id = id;
+		this.description = description;
+		this.price = price;
+	}
+	
+	public UUID getId() { return this.id; }
+	public String getDescription() { return this.description; }
+	public BigDecimal getPrice() { return this.price; }
+
+}
