80ef67cb525a8f01eaf863765a47603a3b05900b
Arthur Rocha <arthurrocha007.net@gmail.com>
2026-01-12 17:34:10 -0300

    feat: adiciona infra - implement jpa

---
+++ src/main/java/com/arthurrocha01/api_java/infra/jpa/JPAItemRepository.java
@@ -0,0 +1,29 @@
+package com.arthurrocha01.api_java.infra.jpa;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.arthurrocha01.api_java.model.item.Item;
+import com.arthurrocha01.api_java.repository.ItemRepository;
+
+@Repository
+public class JPAItemRepository implements ItemRepository{
+	private final SpringItemJPARepository jpa;
+	
+	public JPAItemRepository(SpringItemJPARepository jpa) {
+		this.jpa = jpa;
+	}
+	
+	@Override
+	public List<Item> findAll() {
+		return jpa.findAll()
+				.stream()
+				.map(e -> new Item(
+						e.getId(),
+						e.getDescription(),
+						e.getPrice()
+						))
+				.toList();
+	}
+}
---
+++ src/main/java/com/arthurrocha01/api_java/infra/jpa/SpringItemJPARepository.java
@@ -0,0 +1,11 @@
+package com.arthurrocha01.api_java.infra.jpa;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.arthurrocha01.api_java.infra.jpa.entity.ItemEntity;
+
+public interface SpringItemJPARepository extends JpaRepository<ItemEntity, UUID> {
+
+}
