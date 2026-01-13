8c8aa8b219cbe28e932bd64437d10fcf29c980d2
Arthur Rocha <arthurrocha007.net@gmail.com>
2026-01-13 12:51:11 -0300

    refactor: troca tipo uuif para id autoincremente e Long de Item

--- src/main/java/com/arthurrocha01/api_java/model/Item.java
+++ src/main/java/com/arthurrocha01/api_java/model/Item.java
@@ -1,20 +1,19 @@
 package com.arthurrocha01.api_java.model;
 
 import java.math.BigDecimal;
-import java.util.UUID;
 
 public class Item {
-	private final UUID id;
+	private final Long id;
 	private final String description;
 	private final BigDecimal price;
 	
-	public Item(UUID id, String description, BigDecimal price) {
+	public Item(Long id, String description, BigDecimal price) {
 		this.id = id;
 		this.description = description;
 		this.price = price;
 	}
-	
-	public UUID getId() { return this.id; }
+
+	public Long getId() { return this.id; }
 	public String getDescription() { return this.description; }
 	public BigDecimal getPrice() { return this.price; }
 
